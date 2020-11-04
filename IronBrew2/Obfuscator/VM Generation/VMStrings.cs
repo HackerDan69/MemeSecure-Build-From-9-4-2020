f1bc1b47b77f0a49235fa76e50648a34534599f5
HackerDan69 <73840063+HackerDan69@users.noreply.github.com>
2020-11-03 21:26:24 -0600

    Update VMStrings.cs

--- IronBrew2/Obfuscator/VM Generation/VMStrings.cs
+++ IronBrew2/Obfuscator/VM Generation/VMStrings.cs
@@ -6,7 +6,7 @@ namespace IronBrew2.Obfuscator.VM_Generation
 	public static class VMStrings
     {
 
-        static char[] charactersAvailable = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        static char[] charactersAvailable = {'M', 'Y', 'C', 'O', 'K', 'H', 'U', 'R', 'T', 'Z', 'L', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                                              '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
         public static string GetRandomAlphaNumString(uint stringLength)
